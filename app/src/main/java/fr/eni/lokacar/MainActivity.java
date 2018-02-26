15fde342f3449ce9cbce7e1ecf0c27abf72aed16
guittte <gourmeling@gmail.com>
2018-02-26 15:07:19 +0100

    test 3

--- app/src/main/java/fr/eni/lokacar/MainActivity.java
+++ app/src/main/java/fr/eni/lokacar/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        System.out.println("Titi");
+        System.out.println("Toto");
     }
 }
