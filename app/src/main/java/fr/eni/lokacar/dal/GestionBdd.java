5cd70f97c7bd8de0fd2fc43e66285288110e7bd2
Anais <anais.rousteau@gmail.coml>
2018-02-26 16:15:03 +0100

    bo

--- app/src/main/java/fr/eni/lokacar/dal/GestionBdd.java
+++ app/src/main/java/fr/eni/lokacar/dal/GestionBdd.java
@@ -1,14 +1,55 @@
 package fr.eni.lokacar.dal;
 
 import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 import fr.eni.lokacar.contracts.BddContract;
+import fr.eni.lokacar.contracts.VehiculeContract;
 
-public class GestionBdd {
+public class GestionBdd extends SQLiteOpenHelper {
 
     private static final String TAG = "***GESTIONBDD***";
 
     public GestionBdd(Context context) {
         super(context, BddContract.BDD_NAME, null, BddContract.BDD_VERSION);
+    }
+
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+        //on met la requete sql dans un string
+        String requete = "CREATE TABLE " +
+                VehiculeContract.TABLE_NAME +
+                "(" +
+                VehiculeContract.COL_ID +
+                " INTEGER PRIMARY KEY AUTOINCREMENT," +
+                VehiculeContract.COL_MARQUE +
+                " TEXT, " +
+                VehiculeContract.COL_MODELE +
+                " TEXT, " +
+                VehiculeContract.COL_DESCRIPTION +
+                " TEXT, " +
+                VehiculeContract.COL_IMMATRICULATION +
+                " TEXT, " +
+                VehiculeContract.COL_PRIX +
+                " REAl, " +
+                VehiculeContract.COL_LOUE +
+                " INTEGER, " +
+                VehiculeContract.COL_PHOTO +
+                " BLOB " +
+                ");";
+        //on l'execute
+        sqLiteDatabase.execSQL(requete);
+
+        //on met un log car il est facile de faire des erreurs a cet endroit
+        Log.d(TAG, "Requete : " + requete);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
     }
 }
